06411e2e0a56af22cb9da7c9334c600abcd5ed89
Simillo <simillonakai@gmail.com>
2016-10-29 15:23:32 -0200

    Exercicio 9 100% dredd!

--- lista 5/9.cpp
+++ lista 5/9.cpp
@@ -4,17 +4,34 @@ using namespace std;
 
 int main(){
 	
-	int l,c;
+	int	i, j, x, y, l, c ;
 	
-	cin>>l>>c;
+	cin >> l >> c;
 	
-	int m[l][c];
+	int m[l][c], o[l][c];
 	
-	for(int i = 0; i<l;i++)
-		for(int j = 0; j<c;j++)
-			cin>>m[i][j];
-	
-	int n[l][c];
+	for(i = 0 ; i < l ; i++)
+		for(j = 0 ; j < c ; j++){
+			cin >> o[i][j];
+			m[i][j] = o[i][j];
+ 		}
+
+ 	for(i = 1 ; i < l-1 ; i++){
+ 		for (j = 1 ; j < c-1 ; j++){
+ 			int soma = 0;
+ 			for(x = i - 1 ; x < i + 2 ; x++){
+ 				for(y = j - 1; y < j + 2; y++){
+ 					soma += o[x][y];
+ 				}
+ 			}
+ 			m[i][j] = soma/9;
+ 		}
+ 	}
+ 	for(i = 0 ; i < l ; i++){
+ 		for(j = 0 ; j < c ; j++)
+ 			cout << m[i][j] << " ";
+ 		cout << endl;
+ 	}
 	
 	return 0;
 }
