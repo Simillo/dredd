6303bbd17fdd9bf9f84f8694f142f765f01abca9
Simillo <simillonakai@gmail.com>
2016-10-29 18:34:11 -0200

    Syntax fixes

--- lista 5/9.cpp
+++ lista 5/9.cpp
@@ -16,17 +16,17 @@ int main(){
 			m[i][j] = o[i][j];
  		}
 
- 	for(i = 1 ; i < l-1 ; i++){
- 		for (j = 1 ; j < c-1 ; j++){
+ 	for(i = 1 ; i < l - 1 ; i++)
+ 		for (j = 1 ; j < c - 1 ; j++){
+ 			
  			int soma = 0;
- 			for(x = i - 1 ; x < i + 2 ; x++){
- 				for(y = j - 1; y < j + 2; y++){
+
+ 			for(x = i - 1 ; x < i + 2 ; x++)
+ 				for(y = j - 1; y < j + 2; y++)
  					soma += o[x][y];
- 				}
- 			}
  			m[i][j] = soma/9;
  		}
- 	}
+
  	for(i = 0 ; i < l ; i++){
  		for(j = 0 ; j < c ; j++)
  			cout << m[i][j] << " ";
