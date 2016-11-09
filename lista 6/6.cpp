064295fc22f328c81b82eeb22d154378ac3c0e3c
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-09 14:06:24 -0200

    Funciona mas dredd não aceita

--- lista 6/6.cpp
+++ lista 6/6.cpp
@@ -11,18 +11,15 @@ typedef struct
 } Dados;
 
 int main(){
-/*
+
 	float tempo;
 	Dados d;
 	cin >> d.pi >> d.pf >> d.vi >> d.vf >> tempo;
 
-	if(tempo<1){
-		cout << ((d.vi/(1.0/tempo))+d.pi) << " ";
-		cout << ((d.vf/(1.0/tempo))+d.pf) << endl;
-	}else{
-		cout << ((d.vi/tempo)+d.pi) << " ";
-		cout << ((d.vf/tempo+d.pf)) << endl;
-	}
-*/
+	d.pi = ((d.vi/(1.0/tempo))+d.pi);
+	d.pf = ((d.vf/(1.0/tempo))+d.pf);
+
+	cout<<d.pi<<" " << d.pf << endl;
+
 	return 0;
 }
