ee98c42e2e284360adc5d1c1dc9f064eb5958c12
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-09 21:17:53 -0200

    6.cpp Fix

--- lista 6/6.cpp
+++ lista 6/6.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-
 using namespace std;
+ 
+typedef struct{
+    float p1;
+    float p2;
+} Posicao;
+ 
+typedef struct{
+    float v1;
+    float v2;
+} Velocidade;
 
-typedef struct 
-{
-	float pi;
-	float pf;
-	float vi;
-	float vf;
-} Dados;
+typedef struct{
+    Posicao posicao;
+    Velocidade velocidade;
+} Carro;
 
 int main(){
-
-	float tempo;
-	Dados d;
-	cin >> d.pi >> d.pf >> d.vi >> d.vf >> tempo;
-
-	d.pi = ((d.vi/(1.0/tempo))+d.pi);
-	d.pf = ((d.vf/(1.0/tempo))+d.pf);
-
-	cout<<d.pi<<" " << d.pf << endl;
-
+	
+	Carro carro;
+	    
+	float t;
+	   
+	cin >> carro.posicao.p1 >> carro.posicao.p2 >> carro.velocidade.v1 >> carro.velocidade.v2 >> t;
+	cout << (carro.posicao.p1 + (carro.velocidade.v1 * t)) << " " << (carro.posicao.p2 + (carro.velocidade.v2 * t)) << endl;
+	   
 	return 0;
 }
