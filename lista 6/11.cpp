d7c4e73285530fc45963000fc5abaea59a06fcee
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-10 14:19:29 -0200

    11.cpp nao funcionando

--- lista 6/11.cpp
+++ lista 6/11.cpp
@@ -1,1 +1,41 @@
-ainda fiz
+#include <iostream>
+
+using namespace std;
+
+typedef struct
+{
+	float x;
+	float y;
+} Ponto;
+
+
+int main(){
+	int rota[5];
+	Ponto ponto[5];
+	for(int i=0;i<5;i++)
+		cin >> ponto[i].x >> ponto[i].y;
+
+	for(int i=0;i<5;i++)
+		cin >> rota[i];
+
+
+	float somay,somax,somatotal = 0.0,media;
+	
+	for(int i=1;i<5;i++){
+		somay = (ponto[rota[i]].y - ponto[rota[i-1]].y);
+		somax = (ponto[rota[i]].x - ponto[rota[i-1]].x);
+
+		if(somax==0)
+			media = somay;
+		else
+			media = somay/somax;
+
+		cout << media << endl;
+		
+		somatotal += media;
+	}
+
+	cout << somatotal << endl;
+	
+	return 0;
+}
