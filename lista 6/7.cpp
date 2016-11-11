ae322809ce2242b6ba1bfcb3d76222e4edaf58bf
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-11 14:26:28 -0200

    7 funciona mas nao atende ao dredd | 16 initial commit

--- lista 6/7.cpp
+++ lista 6/7.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 typedef struct{
-    int cod_cliente;
-    char nome_cliente[100];
+    int cod;
+    char nome[100];
 } Cliente;
 
 typedef struct{
-    int cod_cliente;
+    int cod;
     int conta;
     int valor;
 } Compra;
@@ -18,80 +18,87 @@ typedef struct{
 
 int main()
 {
-    int cc = 0,com = 0, n=0,tipo = 1,cod,valor,conta;
+    int tipo = -1, n = 0, cod, conta, valor;
+    
+    int c1=0, c2=0;
+    
     char nome[100];
-    Cliente vetor_cliente[100];
-    Compra vetor_compra[100];
+    
+    Cliente cliente[100];
+    
+    Compra compra[100];
     
     vector<string> resultado;
     
-    while(tipo>=1 && n<100){
+    while(tipo!=0 && n<100){
         cin >> tipo;
-        if(tipo != 0){
+        if(tipo!=0){
             if(tipo == 1){
-                cin >> cod >> nome;
-                
-                int h = 1;
-                for(int i = 0;i<cc;i++){
-                    if(cod == vetor_cliente[i].cod_cliente)
-                        h=0;
+                cin >> cod;
+                int v = 1;
+                for(int i=0;i<c1;i++){
+                    if(cliente[i].cod == cod){
+                        v=0;
+                    }
                 }
-                if(h==1){
-                    vetor_cliente[cc].cod_cliente = cod;
-                    strcpy(vetor_cliente[cc].nome_cliente,nome);
-                    cc++;
+                if(v){
+                    cin >> nome;
+                    cliente[c1].cod = cod;
+                    strcpy(cliente[c1].nome,nome);
+                    c1++;
                     resultado.push_back("OK");
-                }
-                else
+                }else
                     resultado.push_back("ERRO");
             }else if(tipo == 2){
                 cin >> cod;
-                
-                int h = 0;
-                
-                for(int i = 0;i<cc;i++){
-                    if(cod == vetor_cliente[i].cod_cliente){
-                        h=1;
-                        break;
+                int v = 1;
+                for(int i=0;i<c1;i++){
+                    if(cliente[i].cod == cod){
+                        v=0;
                     }
                 }
-                if(!h){
-                	cin >> conta >> valor;
-                    vetor_compra[com].cod_cliente = cod;
-                    vetor_compra[com].conta = conta;
-                    vetor_compra[com].valor = valor;
-                    com++;
-                    resultado.push_back("OK");
-                }else{
+                if(v){
                     resultado.push_back("ERRO");
+                }else{
+                    cin >> conta >> valor;
+                    compra[c2].cod = cod;
+                    compra[c2].conta = conta;
+                    compra[c2].valor = valor;
+                    c2++;
+                    resultado.push_back("OK");
                 }
             }else{
                 cin >> cod;
-                int h=1;
-                
-                for(int i=0;i<cc;i++){
-                    if(cod == vetor_cliente[i].cod_cliente){
-                        vetor_cliente[i].cod_cliente = -1;
-                        char nu[100];
-                        nu[0] = '-';
-                        nu[1] = '1';
-                        strcpy(nu,vetor_cliente[i].nome_cliente);
-                        h=0;
-                        resultado.push_back("OK");
+                int v = 1;
+                for(int i=0;i<c2;i++){
+                    if(compra[i].cod == cod){
+                        if(compra[i].valor > 0){
+                            v=0;
+                        }
                     }
                 }
-                if(h){
+                if(v){
+                    int v = 1;
+                    int index;
+                    for(int i=0;i<c1;i++){
+                        if(cliente[i].cod == cod){
+                            v=0;
+                            index = i;
+                        }
+                    }
+                    if(v){
+                        resultado.push_back("ERRO");
+                    }else{
+                        cliente[index].cod = -1;
+                        resultado.push_back("OK");
+                    }
+                }else{
                     resultado.push_back("ERRO");
                 }
-                
             }
-            
-            n++;
-        }else{
-            break;
         }
+        n++;
     }
-    
     for(int i=0;i<(int)resultado.size();i++)
         cout<<resultado[i]<<endl;
     
