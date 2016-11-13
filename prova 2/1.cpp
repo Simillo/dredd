f3854fc787069446136e4165f09935e5026c7c32
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-13 08:57:17 -0200

    Questão 1 da prova 2, Busca binária + registros

--- prova 2/1.cpp
+++ prova 2/1.cpp
@@ -25,7 +25,7 @@ int main(){
 
 	cod = 1;
 
-	while(cod!=0){
+	while(cod != 0){
 		cin >> cod;
 		if(cod!=0){
 			cin >> cad[c].animal >> cad[c].dono >> cad[c].data.dia >> cad[c].data.mes >> cad[c].data.ano;
@@ -38,16 +38,16 @@ int main(){
 
 	int inicio = 0, meio, fim = c-1, achou=0;
 	while(!achou && inicio <= fim){
-		meio = (inicio+fim)/2;
+		meio = (inicio + fim) / 2;
 		if(busca > cad[meio].cod)
-			inicio = meio+1;
-		else if(busca<cad[meio].cod)
-			fim = meio-1;
+			inicio = meio + 1;
+		else if(busca < cad[meio].cod)
+			fim = meio - 1;
 		else
 			achou = 1;
 	}
 
-	if(inicio>fim || !achou)
+	if(inicio > fim || !achou)
 		cout << "inexistente" << endl;
 	else
 		cout << cad[meio].animal << " " << cad[meio].data.dia << "/" << cad[meio].data.mes << "/" << cad[meio].data.ano << endl;
