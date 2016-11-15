f425fee851621b196ef4347babaa10e239d0a8ab
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2016-11-15 17:06:15 -0200

    7 feito, porém não enviado (enunciado vago)

--- lista 7/7.cpp
+++ lista 7/7.cpp
@@ -4,11 +4,43 @@
 
 using namespace std;
 
+typedef struct
+{
+	char nome[100];
+	int telefone;
+} Dados;
 
+vector<Dados> v;
+Dados aux;
+
+void man(Dados a){
+	int t = v.size();
+	if(t==0)
+		v.push_back(a);
+	else{
+		int p = 1;
+		for(int i=0;i<t;i++)
+			if(strcmp(a.nome,v[i].nome)<0){
+				v.insert(v.begin()+i,a);
+				p=0;
+				break;		
+			}
+		if(p)
+			v.push_back(a);
+	}
+}
 
 int main(){
+	int n;
+	cin >> n;
 
-	char n1[100],n2[100];
+	for (int i = 0; i < n; i++){
+		cin >> aux.nome >> aux.telefone;
+		man(aux);
+		cout << endl;
+		for(int i=0;i<(int)v.size();i++)
+			cout << v[i].nome << " " << v[i].telefone << endl;
+	}
 
 	return 0;
 }
