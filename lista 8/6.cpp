d58dbebf2a293c3fd0ad0a73591d688c0369c886
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2017-01-25 15:42:29 -0200

    100% + código usando bitshift

---
+++ lista 8/6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int t(int n){
+	return n<2?1:2*t(n-1)+1;
+}
+
+int main(){
+	int n;
+	cin >> n;
+
+	cout << t(n) << endl;
+}
+
+/*
+
+Código usando bitwise.
+
+#include <iostream>
+
+int main(){
+	int n;
+	std::cin >> n;
+	n = (1<<n)-1;
+	std::cout << n << std::endl;
+	return 0;
+}
+
+*/
