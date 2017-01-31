e0b605bdb4f2fed51430dcd2eb550b4493e880f7
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2017-01-30 23:07:16 -0200

    100%

--- lista 8/15.cpp
+++ lista 8/15.cpp
@@ -2,17 +2,14 @@
 
 using namespace std;
 
-
-
-void f(int n, int q){
-	cin >> n;
-	if(--q) f(n,q);
-	cout << n << " ";
+void f(int n, int q) {
+    cin >> n;
+    if (--q) f(n, q);
+    cout << n << " ";
 }
-int main()
-{
-	int q;
-	cin >> q;
-	f(0,q);
+int main() {
+    int q;
+    cin >> q;
+    f(0, q);
     return 0;
 }
