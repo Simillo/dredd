999495a408f805fa2d88c20fa8c6734fd89ca6db
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2017-02-01 21:11:03 -0200

    funcionando mas não consigo fazer retornar

--- lista 8/16.cpp
+++ lista 8/16.cpp
@@ -1,68 +1,53 @@
 #include <iostream>
 #include <vector>
 
+#define fi for(int i=0;i<10;i++)
+#define fj for(int j=0;j<15;j++) 
+
 using namespace std;
 
-int around(vector<vector<char> > m, int l, int c){
-	if(l>0 && c > 0)
-		if(m[l-1][c-1] == '#')
-			return 0;
-	else if(l<10 && c<15)
-		if(m[l+1][c+1] == '#')
-			return 0;
-	else if(l>0 && c == 0)
-		if(m[l-1][c] == '#')
-			return 0;
-	else if(l==0 && c>0)
-		if(m[l][c-1] == '#')
-			return 0;
-	else if(l < 10 && c==15)
-		if(m[l+1][c] == '#')
-			return 0;
-	else if(l == 10 && c<15)
-		if(m[l][c+1] == '#')
-			return 0;
-	return 1;
-}
 vector<vector<char> > path(vector<vector<char> > m, int l, int c){
-	if(l<10&&c<15){
-		if(m[l][c] == '.'){
-			if(around(m,l,c))
-				m[l][c] = 'x';
-			else if(l<10&&c==15)
-				path(m,++l,0);
-			else if(l==10&&c<15)
-				path(m,l,++c);
-		}
+	if(l == 9 && c == 14){
+		m[l][c] = 'x';
+	}else if(m[l][c] == '.'){
+		m[l][c] = 'x';
+		if(m[l][c+1] == '.')
+			path(m,l,++c);	
+		else if(m[l+1][c] == '.')
+			path(m,++l,c);
+		else
+			path(m,0,0);
+	}else if(m[l][c] == 'x'){
+		if(m[l+1][c] == 'x')	
+			path(m,++l,--c);
+		if(m[l][c+1] == 'x')
+			path(m,l,++c);
 	}
-	return m;
-
 }
 
 
 
 int main()
 {
-	vector<vector<char> > m;
-	vector<vector<char> > newm;
-	for(int i=0;i<10;i++){	
-		vector<char> linha;
-		for(int j=0;j<15;j++){
-			char coluna;
-			cin >> coluna;
-			linha.push_back(coluna);
-		}
-		m.push_back(linha);
-	}
+    vector<vector<char> > matrix;
+    vector<vector<char> > newMatrix;
+    
+    fi{
+        vector<char> linhaAux;
+        fj{
+            char itemAux;
+            cin >> itemAux;
+            linhaAux.push_back(itemAux);
+        }
+        matrix.push_back(linhaAux);
+    }
+    newMatrix = path(matrix,0,0);
 
-	newm = path(m,0,0);
-	cout << endl << endl << endl;
-	for(int i=0;i<10;i++){	
-		for(int j=0;j<15;j++){
-			cout << newm[i][j];
+	fi{
+		fj{
+			cout << newMatrix[i][j];
 		}
 		cout << endl;
 	}
-	
-	return 0;
+    return 0;
 }
