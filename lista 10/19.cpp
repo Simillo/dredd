eb93e3c044c8c80b0fa22adf157271dba966e62f
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2017-02-16 21:52:17 -0200

    100%

--- lista 10/19.cpp
+++ lista 10/19.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 typedef struct {
-    float x,y;
+    string nome;
+    float x,y,preco;
     float dist;
 } Posi;
 int main(){
@@ -15,8 +16,33 @@ int main(){
     cin >> n;
     Posi v[n];
     for(int i=0;i<n;i++){
-        cin >> v[i].x >> v[i].y;
-        v[i].dist = sqrt(pow(posx,v[i].x,2)+pow(posy,v[i].y,2)); 
+        cin >> v[i].nome >> v[i].x >> v[i].y >> v[i].preco;
+        v[i].dist = sqrt(pow(posx-v[i].x,2)+pow(posy-v[i].y,2)); 
     }
+    for(int i=0;i<n;i++)
+	for(int j=i+1;j<n;j++)
+	   if(v[i].dist > v[j].dist){
+	   	float aux = v[i].dist;
+	   	v[i].dist = v[j].dist;
+	   	v[j].dist = aux;
+	   	
+		aux = v[i].x;
+		v[i].x = v[j].x;
+		v[j].x = aux;
+		aux = v[i].y;
+		v[i].y = v[j].y;
+		v[i].y = aux;
+		aux = v[i].preco;
+		v[i].preco = v[j].preco;
+		v[j].preco = aux;
+		string nom = v[i].nome;
+		v[i].nome = v[j].nome;
+		v[j].nome = nom;
+	   }
+	float sum = 0;
+    for(int i=0;i<a;i++)
+	sum+=v[i].preco;
+    cout << sum << endl;
+
     return 0;
 }
