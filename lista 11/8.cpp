00af2d56693150b7d77cd8a9a1c4f2576266ce75
Simillo D'Antona Zagury Nakai Ferreira <simillonakai@gmail.com>
2017-03-14 21:56:07 -0300

    100%

--- lista 11/8.cpp
+++ lista 11/8.cpp
@@ -5,36 +5,46 @@ using namespace std;
 typedef struct {
     int mat;
     float notas[2];
+    float media;
 } Alunos;
 
 int main(){
     int n;
     cin >> n;
 
-    Alunos* a = new Alunos [n];
-
     Alunos** m = new Alunos *[n];
 
     for(int i=0;i<n;i++)
         m[i] = new Alunos [n];
 
-    int aux = 0;
     for(int j=0;j<n;j++){
         for(int x=0;x<n;x++){
-            cin >> a[aux].mat >> a[aux].notas[0] >> a[aux].notas[1];
-            m[j][x] = a[aux];
-            aux++;
-            //  cout << "-----------" << endl  << "-> "<< aux << endl << "------------" << endl; 
+            Alunos* al = new Alunos();
+            cin >> al->mat >> al->notas[0] >> al->notas[1];
+            al->media = (al->notas[0]+al->notas[1])/2;
+            m[j][x] = *al;
         }
-    }  
+    }
+
+for(int col = 0; col < n; col++)
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            if(m[i][col].media < m[j][col].media){
+                int auxT = m[i][col].mat;
+                m[i][col].mat = m[j][col].mat;
+                m[j][col].mat = auxT;
+                float mediaT = m[i][col].media;
+                m[i][col].media = m[j][col].media;
+                m[j][col].media = mediaT; 
+            }
     
     for(int j=0;j<n;j++){
         for(int x=0;x<n;x++){
             Alunos aluno = m[j][x];
-            int maux = aluno.mat;
-            float n1 = aluno.notas[0];
-            float n2 = aluno.notas[1];
-            cout << maux << " " << n1 << " " << n2;
+            int mat = aluno.mat;
+            // float me = aluno.media;
+            
+            cout << mat <<  " " ;
         }
         cout << endl;
     }
